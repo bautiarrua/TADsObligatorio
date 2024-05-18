9353996693655e7bca216f9f663101ef101c17d7
ARRUABARRENA MUÑOZ Juan Bautista <jarruabarrena@correo.um.edu.uy>
2024-05-18 13:27:11 -0300

    Nuevas Pruebas

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.LinkedList;
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<Integer> lista = new LinkedList<>();
+        lista.add(10);
+        System.out.println(lista.get(0));
+        lista.add(200);
+    }
+
+}
