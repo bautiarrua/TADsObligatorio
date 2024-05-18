b3257b770e9fd156587e21f0c0316ce5728ecb03
ARRUABARRENA MUÑOZ Juan Bautista <jarruabarrena@correo.um.edu.uy>
2024-05-18 13:38:36 -0300

    Pruebo cambios

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,12 @@ public class Main {
     public static void main(String[] args) {
         LinkedList<Integer> lista = new LinkedList<>();
         lista.add(10);
-        System.out.println(lista.get(0));
         lista.add(200);
+        lista.add(300);
+        lista.add(100);
+        for(int i = 0; i<lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
     }
 
 }
