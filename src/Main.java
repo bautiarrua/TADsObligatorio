70c1e01267a1733684d65aa43151a81e5db4cdac
Maluco130 <franconre2004@gmail.com>
2024-05-20 09:48:03 -0300

    Los imports de junit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,11 @@ import adt.arbolbinario.Arbolbinario;
 import adt.linkedlist.MyLinkedListImpl;
 import adt.linkedlist.MyList;
 import adt.linkedlist.Node;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+
 public class Main {
     public static void main(String[] args) {
         Arbolbinario<Integer, Integer> oTree = new Arbolbinario<>();
