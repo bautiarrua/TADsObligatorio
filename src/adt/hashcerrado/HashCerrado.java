b92aa4c2a58392b25ecabb2a12cbb59d90d7e536
bautiarrua <bautista.arruabarrena@gmail.com>
2024-05-20 10:47:36 -0300

    carpetahash

---
+++ src/adt/hashcerrado/HashCerrado.java
@@ -0,0 +1,14 @@
+package adt.hashcerrado;
+
+public class HashCerrado<V> implements MyHashCerrado<V> {
+
+    public void insert(V valor) {
+        
+    }
+    public void serch(V valor) {
+
+    }
+    public void delate(V valor) {
+
+    }
+}
---
+++ src/adt/hashcerrado/MyHashCerrado.java
@@ -0,0 +1,8 @@
+package adt.hashcerrado;
+
+public interface MyHashCerrado<V> {
+    void insert(V valor);
+    void serch(V valor);
+    void delate(V valor);
+
+}
