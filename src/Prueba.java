8f782ff84dc4449864f0fd10737ac78cc015e22d
Maluco130 <franconre2004@gmail.com>
2024-05-20 10:48:32 -0300

    Clsae prueba

---
+++ src/Prueba.java
@@ -0,0 +1,28 @@
+import adt.arbolbinario.Arbolbinario;
+import adt.linkedlist.MyLinkedListImpl;
+import adt.linkedlist.MyList;
+import adt.linkedlist.Node;
+
+import org.junit.jupiter.api.Assertions.*;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Prueba {
+
+    public void lista(){
+
+    }
+    public void stack(){
+
+    }
+    public void queue(){
+
+    }
+    public void arbol(){
+
+    }
+    public void hash(){
+
+    }
+}
+
