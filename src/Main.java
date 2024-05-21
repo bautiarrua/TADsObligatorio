954e2bff96c1a23e1ed0db7d820a1fbd56422401
bautiarrua <bautista.arruabarrena@gmail.com>
2024-05-21 09:15:08 -0300

    Primeras Pruebas Unitarias

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import adt.linkedlist.MyLinkedListImpl;
 import adt.linkedlist.MyList;
 import adt.linkedlist.Node;
 
+import adt.stack.EmptyStackException;
 import org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.Test;
@@ -10,7 +11,11 @@ import org.junit.Test;
 public class Main {
     public static void main(String[] args) {
         Prueba MiPrueba = new Prueba();
-
+        MiPrueba.stack_push();
+        try {
+            MiPrueba.stack_pop();
+        }catch (EmptyStackException e){};
+        MiPrueba.stack_peek();
 
     }
 
--- src/Prueba.java
+++ src/Prueba.java
@@ -2,28 +2,45 @@ import adt.arbolbinario.Arbolbinario;
 import adt.linkedlist.MyLinkedListImpl;
 import adt.linkedlist.MyList;
 import adt.linkedlist.Node;
+import adt.stack.EmptyStackException;
+import adt.stack.MyStack;
 
-import org.junit.jupiter.api.Assertions.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Prueba {
 
-    public void lista(){
-
+    public void testSizeEmptyStack() {
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        assertEquals(0, stack.size());
     }
-    public void stack(){
 
-
+    public void stack_push(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(2);
+        stack.push(3);
+        assertEquals(2,stack.size());
     }
-    public void queue(){
-
+    public void stack_pop() throws EmptyStackException{
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(2);
+        stack.push(3);
+        stack.pop();
+        assertEquals(1,stack.size());
+        stack.pop();
+        assertEquals(0,stack.size());
     }
-    public void arbol(){
-
+    public void stack_peek(){
+        MyStack<Integer> stack = new MyLinkedListImpl<>();
+        stack.push(2);
+        stack.push(3);
+        assertEquals(2,stack.size());
+        assertEquals(Integer.valueOf(3),stack.peek());
     }
-    public void hash(){
 
-    }
+
 }
 
